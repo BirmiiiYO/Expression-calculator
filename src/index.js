b2273b0449012b1348a3168f92b2b477f47a2eb1
BirmiiiYo <avramenkoa773@gmail.com>
2022-10-31 10:49:37 +0300

    test new alias at git

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Context from './store';
-
+// func
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
