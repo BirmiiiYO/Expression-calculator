654ee811830ac6393b864d079d3aa744e69ad573
BirmiiiYo <avramenkoa773@gmail.com>
2022-12-25 13:05:52 +0300

    add constants for styles

---
+++ src/styles/consts.js
@@ -0,0 +1,32 @@
+const FONT_SIZE = {
+  XS: 12,
+  S: 16,
+  M: 20,
+  L: 24,
+  XL: 32,
+}
+const BORDER_RADIUS = {
+  XS: 4,
+  S: 6,
+  M: 8,
+  L: 10,
+  CIRCLE: 50,
+}
+
+const FONT_WEIGHT = {
+  XS: 300,
+  S: 400,
+  M: 500,
+  L: 600,
+  XL: 800,
+}
+
+const RANGE = {
+  XS: 5,
+  S: 10,
+  M: 20,
+  L: 30,
+  XL: 50,
+}
+
+export { BORDER_RADIUS, FONT_SIZE, FONT_WEIGHT, RANGE }
